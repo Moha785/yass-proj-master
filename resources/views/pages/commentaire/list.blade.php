4c75e5b09c5cb6e01c42f28f2f774981781a5402
Moha785 <mohamed.kherroub78@gmail.com>
2024-07-22 14:00:27 +0200

    push final

---
+++ resources/views/pages/commentaire/list.blade.php
@@ -0,0 +1,45 @@
+<!-- list of ressources  -->
+@extends('layouts.default')
+@section('content')
+<div id="" class="mt-8 ">
+
+    <div class="flex flex-col gap-2 justify-center ">
+        <h2 class="text-violet-500 text-xl md:text-2xl font-bold px-2"> Liste de tout les commentaires du site   </h2>
+
+     
+        
+        
+        {{ $commentaires->links() }}
+        
+        <!-- cadre de tous  les contenus  -->
+        <div class="w-full min-h-screen flex flex-row gap-4 flex-wrap justify-between  items-start text-sm text-violet-500  "> 
+            @forelse($commentaires as $index => $row)
+                <!-- Le cadre d'une ressource  -->
+                <div class="w-80 mt-1 flex flex-col gap-1 ">
+                            
+                            <div class="flex flex-col gap-1  items-center mt-1 space-y-0 ">
+                                <h1 class="font-bold text-violet-500 text-md leading-tight">{{ $row->id }} - {{ $row->user->name}}</h1>
+                                <a href="{{route('ressource', ['id'=>$row->ressource->id])}}"><h2 class="font-bold text-violet-500 text-md leading-tight">{{ $row->ressource->titre}}</h2></a>
+                                <p class=" mt-4 mb-4 text-slate-900 border-b border-t border-slate-300 ">   {{ Str::limit($row->corps, 50) }} </p>
+                                <p class=" mt-4 mb-4 text-slate-900 border-b border-t border-slate-300 ">   {{ $row->created_at}} </p>
+                                <form action="{{ route('statecomm') }}"  method="POST">
+                                    @csrf
+                                    <input type="hidden" name="id" value="{{$row->id}}">
+                                    <button type="submit" class="bg-violet-500 text-white p-2 rounded-md">
+                                        @if ($row->isactif === 0) Activer
+                                        @else Désactiver
+                                        @endif
+                                    </button>
+                                </form>
+                            </div>  
+                </div>
+
+            @empty <p class="text-violet-500 text-justify"> Aucune commentaire trouvé !!</p>
+            @endforelse
+        </div>
+
+    </div>    
+
+</div>
+
+@endsection
---
+++ resources/views/pages/user/user.blade.php
@@ -0,0 +1,23 @@
+   @extends('layouts.default')
+   @section('content')
+         <div class="container">
+         
+               <ul class="ml-2 flex flex-col gap-6 text-gray-500 w-[800px]">
+                     <h2 class="text-3xl font-bold" > Liste des utilisateurs du site web </h2>
+                     <div class="text-start mb-2"> <button href="" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" disabled>Ajouter un utilisateur</button>  </div>
+                     @forelse($users as $index => $row)                     
+                              <ul class="ml-2 pt-2 flex flex-row border-b-0 gap-6 justify-between  items-center" > 
+                                 <li  > {{ $index + 1 }} </li>
+                                 <li  > {{ $row->name }} </li>
+                                 <li > {{ $row->email }}</li>     
+                                 <li > {{ $row->groupe->lib }}</li>                         
+                                 <li > <button href="" class="bg-blue-500 hover:bg-blue-700 text-white font-bold rounded p-2" disabled>Editer</button></li>
+                                 <li > <button href="" class="bg-red-600 hover:bg-red-300 text-white font-bold  rounded p-2" disabled >Desactiver le compte</button></li>
+
+                              </ul>
+                     @empty <span> No Users Found</span>
+                     @endforelse
+               </ul>
+         </div>
+ 
+   @endsection
